fa64b6cbdf294d662a8362a799fdd3960c1ca40d
Vincent Chang <vincentcch79@gmail.com>
2016-07-25 15:29:15 +0800

    finished ratingAction view

--- BanannyDemo/RatingActionViewController.swift
+++ BanannyDemo/RatingActionViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class RatingActionViewController: UIViewController {
 
+    @IBOutlet weak var sendRatingButton: UIButton!
+    @IBOutlet weak var ratingActionBackground: UIView!
+    @IBOutlet weak var ratingScrollBackground: UIScrollView!
     @IBOutlet weak var ratingActionContentView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "請完成評價"
+        ratingActionBackground.backgroundColor = UIColor(red: 246/255, green: 246/255, blue: 246/255, alpha: 1)
+        ratingScrollBackground.backgroundColor = UIColor(red: 246/255, green: 246/255, blue: 246/255, alpha: 1)
+        sendRatingButton.backgroundColor = UIColor(red: 99/255, green: 108/255, blue: 163/255, alpha: 1)
+        
+        
         
         //shadow
-        ratingActionContentView.layer.shadowColor = UIColor(white: 0, alpha: 1).CGColor
-        ratingActionContentView.layer.shadowOffset = CGSize(width: 0, height: 5)
+        ratingActionContentView.layer.shadowColor = UIColor(white: 0, alpha: 0.22).CGColor
+        ratingActionContentView.layer.shadowOffset = CGSize(width: 0, height: 2)
         ratingActionContentView.layer.shadowOpacity = 0.5
         
         // Do any additional setup after loading the view.
