8cd76c09f4b2f03b659aa536d9c9a4d729a58f67
Vincent Chang <vincentcch79@gmail.com>
2016-07-25 18:10:07 +0800

    fix textfield padding

--- BanannyDemo/ParentDashboardViewController.swift
+++ BanannyDemo/ParentDashboardViewController.swift
@@ -254,4 +254,19 @@ class ParentDashboardViewController: UIViewController, UIPickerViewDelegate, UIP
 
 }
 
-
+class TextField: UITextField {
+    
+    let padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 5);
+    
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRectForBounds(bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+}
